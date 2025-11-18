65c1e123214fcf23bbff7c649c3dab84ec567d8b
Amr-shousha <131431873+Amr-shousha@users.noreply.github.com>
2025-11-18 16:30:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/eat-and-split/",
+  base: "/eat-and-split--React/",
 
   plugins: [react()],
 });
